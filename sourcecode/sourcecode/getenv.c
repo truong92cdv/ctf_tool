480672d84fbc8ec38356c68954c2834cd0720e68
root <root@localhost.localdomain>
2019-02-28 23:56:31 +0700

    update

--- sourcecode/sourcecode/getenv.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-int main() {
-	printf("%08x\n", getenv("EGG"));
-	return 0;
-}
--- sourcecode/sourcecode/helloworld.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-int main() {
-	printf("You have been hacked!\n");
-	return 0;
-}
