cf17c6f70ecfd87b58c30af3972ea9f3dcfb8cf2
root <root@localhost.localdomain>
2019-03-29 21:58:36 +0700

    update

--- CTF_source/SecurinetsCTF_2019/Reverse/Matrix_of_hell/solve.py
+++ CTF_source/SecurinetsCTF_2019/Reverse/Matrix_of_hell/solve.py
@@ -1,44 +1,17 @@
-import string
-
-something = list('_'*120)
+something = [65, '_', '_', '_', 66, '_', '_', '_', 67, '_', '_', '_', 68, '_', '_', '_', 69, '_', '_', '_', '_', '_', '_', '_', 70, '_', '_', '_', 71, '_', '_', '_', 72, '_', '_', '_', 73, '_', '_', '_', 75, '_', '_', '_', '_', '_', '_', '_', 76, '_', '_', '_', 77, '_', '_', '_', 78, '_', '_', '_', 79, '_', '_', '_', 80, '_', '_', '_', '_', '_', '_', '_', 81, '_', '_', '_', 82, '_', '_', '_', 83, '_', '_', '_', 84, '_', '_', '_', 85, '_', '_', '_', '_', '_', '_', '_', 86, '_', '_', '_', 87, '_', '_', '_', 88, '_', '_', '_', 89, '_', '_', '_', 90, '_', '_', '_', '_', '_', '_', '_']
+STRING  = 'B0C2A2C6A3A7C5@6B5F0A4G2B5A2'
 ENCODED = '_'*28
-STRING = '_'*28
-FINAL = '_'*18
-PASSWORD = 'FACEBOOKISEVIL'
+PASSWORD = '_'*14
 
-k = 0
-i = 0
-while (i < 5):
-	j = 0
-	while (j < 5):
-		if (k == 9):
-			j -= 1
-		else:
-			something[(6*i+j)*4] = k + 0x41
-		k += 1
-		j += 1
-	i += 1
-
-print something
-
-print PASSWORD
-
-t = 0
-length = len(PASSWORD)
-
-for c in range(length):
-	for i in range(5):
-		for j in range(5):
-			if something[(6*i+j)*4] == ord(PASSWORD[c]):
-				ENCODED = ENCODED[:t] + chr(ord('A')+i) + ENCODED[t+1:]
-				ENCODED = ENCODED[:t+1] + chr(ord('1')+j) + ENCODED[t+2:]
-				t += 2
-
+# calculate ENCODED
+for c in range(len(STRING)):
+	ENCODED = ENCODED[:c] + chr(ord(STRING[c]) ^ (c & 3)) + ENCODED[c+1:]
 print ENCODED
 
-for c in range(len(ENCODED)):
-	STRING = STRING[:c] + chr(ord(ENCODED[c]) ^ (c & 3)) + STRING[c+1:]
-
-print STRING
-
+# calculate PASSWORD
+for c in range(len(PASSWORD)):
+	i = ord(ENCODED[c*2]) - ord('A')
+	j = ord(ENCODED[c*2+1]) - ord('1')
+	PASSWORD = PASSWORD[:c] + chr(something[(6*i+j)*4]) + PASSWORD[c+1:]
+print PASSWORD
 
---
+++ CTF_source/SecurinetsCTF_2019/Reverse/Matrix_of_hell/test.py
@@ -0,0 +1,47 @@
+import string
+
+something = list('_'*120)
+ENCODED = '_'*28
+STRING = '_'*28
+FINAL = '_'*18
+PASSWORD = 'NOSYSTEMISSAFE'
+
+# calculate something
+k = 0
+i = 0
+while (i < 5):
+	j = 0
+	while (j < 5):
+		if (k == 9):
+			j -= 1
+		else:
+			something[(6*i+j)*4] = k + 0x41
+		k += 1
+		j += 1
+	i += 1
+
+print something
+
+print PASSWORD
+
+# calculate ENCODED
+t = 0
+length = len(PASSWORD)
+
+for c in range(length):
+	for i in range(5):
+		for j in range(5):
+			if something[(6*i+j)*4] == ord(PASSWORD[c]):
+				ENCODED = ENCODED[:t] + chr(ord('A')+i) + ENCODED[t+1:]
+				ENCODED = ENCODED[:t+1] + chr(ord('1')+j) + ENCODED[t+2:]
+				t += 2
+
+print ENCODED
+
+# calculate STRING
+for c in range(len(ENCODED)):
+	STRING = STRING[:c] + chr(ord(ENCODED[c]) ^ (c & 3)) + STRING[c+1:]
+
+print STRING
+
+
